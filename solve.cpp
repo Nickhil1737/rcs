b3a2c451d0e883c06a36428c9181157ca842c7c7
Nickhil1737 <58224547+Nickhil1737@users.noreply.github.com>
2021-04-06 17:16:41 +0530

    Update solve.cpp

--- solve.cpp
+++ solve.cpp
@@ -9,7 +9,7 @@ typedef std::pair<int,int> ii;
 #define debgg(x,y)	std::cerr<<(#x)<<" => "<<x<<'\t'<<(#y)<<' '<<y<<'\n';
 #define len(a) (int)(a).size()
 #define all(x) x.begin(),x.end()
-const int mod = 10000'0000'7;
+const int mod = 1'0000'0000'7;
 //const int mod = 998244353;
 
 bool comp (int x, int y) {
@@ -34,12 +34,13 @@ int arr[maxn];
 
 void solve ()
 {
+    ans = 0;
     bool ok = false;
     int n,m,k,q;  std::cin>>n;
+    //string  s;  cin>>s;
     for (int i = 0; i < n;++i) {
         std::cin>>arr[i];
     }
-    ans = 0;
     std::cout<<(ok?"YES":"NO");
     std::cout<<'\n';
 }
@@ -52,9 +53,22 @@ int main ()
 {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(0);
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    //cout << setprecision(15) << fixed;
     int testcases = 1;  
     std::cin>>testcases;
     for (int t = 0; t < testcases; ++t)
         solve();
+
+		auto end = std::chrono::high_resolution_clock::now();
+		cerr << setprecision(4) << fixed;
+	    cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
     return 0;
 }
+
+
+/*
+Tuesday 06 April 2021 05:17:23 PM IST
+     author:   __astrainL3gi0N
+*/
