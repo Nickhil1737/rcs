eebb1f6d80ba8039d215f06bde82c0c25f98dfb0
Nickhil1737 <58224547+Nickhil1737@users.noreply.github.com>
2021-07-05 21:47:41 +0530

    Create cp.sh

---
+++ cp.sh
@@ -0,0 +1,10 @@
+touch solve.cpp
+echo -e "\n/*" > solve.cpp
+echo -e "\t\t\t$(date)" >> solve.cpp
+echo -e "\t\t\t\t\t@uthor::dpac" >> solve.cpp
+echo -e "*/\n" >> solve.cpp
+cat /home/astrainlegion/code/codeforces/solve.cpp >> solve.cpp
+cat /home/astrainlegion/code/codeforces/gen.cpp > gen.cpp
+cat /home/astrainlegion/code/codeforces/script.sh > script.sh
+vim solve.cpp
+
